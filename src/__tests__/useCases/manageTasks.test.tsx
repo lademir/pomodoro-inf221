fddc57471285c394f2377a220031313f206a7299
lademir <lademirjunior@hotmail.com>
2022-07-20 00:02:36 -0300

    first commit

---
+++ src/__tests__/useCases/manageTasks.test.tsx
@@ -0,0 +1,59 @@
+import { PomodoroInterface } from "../../core/PomodoroInterface";
+import { TaskInterface } from "../../core/TaskInterface";
+import { Pomodoro } from "../../domain/models/Pomodoro";
+import { Task } from "../../domain/models/Task";
+import { TaskWithNullFieldsException } from "../../domain/useCases/exceptions/TaskWithNullFieldsException";
+import { ManageTask } from "../../domain/useCases/manageTask";
+
+// adicionar, remover, editar e finalizar tarefa
+
+const TASK_NAME = "mockTask";
+const TASK_DESCRIPTION = "mockDescription";
+
+describe('ManageTaskUsecase', () => {
+
+    let pomodoroSUT: PomodoroInterface;
+
+    beforeEach(() => {
+        pomodoroSUT = Pomodoro.create();
+    });
+
+    //  ADICIONAR
+
+    it('should add new task to pomodoro', () => {
+        const manageTask = new ManageTask();
+        expect(pomodoroSUT.tarefas.length).toBe(0);
+
+        const mockTask = Task.create(TASK_NAME, TASK_DESCRIPTION);
+        manageTask.add(mockTask, pomodoroSUT);
+
+        expect(pomodoroSUT.tarefas.length).toBe(1);
+    });
+
+    it('should not add task with null description', () => {
+        const manageTask = new ManageTask();
+
+        const mockTaskNullDescription = Task.create(TASK_NAME, "");
+        expect(() => manageTask.add(mockTaskNullDescription, pomodoroSUT)).toThrowError(TaskWithNullFieldsException);
+    });
+
+    it('should not add task with null name', () => {
+        const manageTask = new ManageTask();
+
+        const mockTaskNullDescription = Task.create("", TASK_DESCRIPTION);
+        expect(() => manageTask.add(mockTaskNullDescription, pomodoroSUT)).toThrowError(TaskWithNullFieldsException);
+    });
+
+    it('should not add task with null fields', () => {
+        const manageTask = new ManageTask();
+
+        const mockTaskNullDescription = Task.create("", "");
+        expect(() => manageTask.add(mockTaskNullDescription, pomodoroSUT)).toThrowError(TaskWithNullFieldsException);
+    });
+
+    // EDITAR
+
+    // FINALIZAR
+
+    // REMOVER
+});
